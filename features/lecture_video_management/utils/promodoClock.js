ab843a810186c8d47e6c72d00e0567e8f58c269d
Do-Bach-2006 <dobachk5@gmail.com>
2024-11-20 12:37:56 +0700

    add functions to the promodo clock
    this patch implement the new function to the promodo clock including
    update the UI when the clock runs

--- features/lecture_video_management/utils/promodoClock.js
+++ features/lecture_video_management/utils/promodoClock.js
@@ -1,21 +1,85 @@
 const Timer = require("timer-for-pomodoro");
 
 // Create a Timer instance with desired time values
-const timer = new Timer(25, 5, 4); // 25 minutes of work, 5 minutes of break, 4 rounds
+let timer = new Timer(25, 5, 4); // 25 minutes of work, 5 minutes of break, 4 rounds
+let stopState = true;
+
+// set up input validation
+const inputs = document.querySelectorAll(".time-character");
+
+inputs.forEach((input) => {
+  input.addEventListener("input", function (e) {
+    // Remove non-numeric characters and limit to 2 digits
+    e.target.value = e.target.value.replace(/\D/g, "").slice(0, 2);
+  });
+});
+
+function get_time() {
+  const workMinuiteElement = document.getElementById("workMinutes");
+  const restMinuiteElement = document.getElementById("restMinutes");
+  const roundsElement = document.getElementById("rounds");
+
+  const DEFAULT_WORK_MINUTES = 25;
+  const DEFAULT_REST_MINUTES = 5;
+  const DEFAULT_ROUNDS = 4;
+
+  const workMinutes =
+    workMinuiteElement.value == ""
+      ? DEFAULT_WORK_MINUTES
+      : workMinuiteElement.value;
+  const restMinutes =
+    restMinuiteElement.value == ""
+      ? DEFAULT_REST_MINUTES
+      : restMinuiteElement.value;
+  const rounds =
+    roundsElement.value == "" ? DEFAULT_ROUNDS : roundsElement.value;
+
+  const newTimer = new Timer(workMinutes, restMinutes, rounds);
+
+  // Subscribe to timer events
+  newTimer.subscribe((currentTime) => {
+    // TODO: change the UI here
+    const timerMinuiteElement = document.getElementById("timerMinuite");
+    const timerSecondElement = document.getElementById("timerSecond");
+    console.log("hello");
+    // console.log(currentTime);
+    console.log(timerMinuiteElement);
+    console.log(timerSecondElement);
+    timerMinuiteElement.textContent = currentTime.minutes;
+    timerSecondElement.textContent = currentTime.seconds;
+  });
+
+  return newTimer;
+}
 
 // Subscribe to timer events
 timer.subscribe((currentTime) => {
-  // TODO: change the UI here
+  const timerMinuiteElement = document.getElementById("timerMinuite");
+  const timerSecondElement = document.getElementById("timerSecond");
+  console.log("hello");
+  // console.log(currentTime);
+  console.log(timerMinuiteElement);
+  console.log(timerSecondElement);
+  timerMinuiteElement.textContent = currentTime.minutes;
+  timerSecondElement.textContent = currentTime.seconds;
 });
 
-// TODO: implement the clocks
-
 function start_timer() {
+  timer = get_time();
   timer.start();
+  stopState = false;
 }
 
+// TODO: change the button to continue when user hit the pause button
+// FIXME: the next state is not working
 function stop_timer() {
-  timer.pause();
+  if (!stopState) {
+    stopState = true;
+    timer.pause();
+  } else {
+    stopState = false;
+    timer.start();
+  }
 }
 
 function next_state() {
@@ -25,5 +89,3 @@ function next_state() {
 function reset_timer() {
   timer.stop();
 }
-
-function get_time() {}
